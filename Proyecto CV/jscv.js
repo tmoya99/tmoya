9c92b31049543b74e554c83bf1bfd6731224cdf3
tmoya99 <123680964+tmoya99@users.noreply.github.com>
2023-01-26 17:56:53 -0300

    Add files via upload

---
+++ Proyecto CV/jscv.js
@@ -0,0 +1,103 @@
+
+
+document.getElementById('masinfouca').onclick=function(){
+    console.log("Mostrar info de la carrera de la UCA")
+    document.getElementById('detallesacademicosuca').style.display = 'block';
+    document.getElementById('masinfouca').style.display = 'none';
+    }
+
+document.getElementById('menosinfouca').onclick=function(){
+        console.log("Ocultar info de la carrera de la UCA")
+        document.getElementById('detallesacademicosuca').style.display = 'none';
+        document.getElementById('masinfouca').style.display = 'block';
+    }
+    
+document.getElementById('masinfobrighton').onclick=function(){
+        console.log("Mostrar info de la carrera del colegio Brighton")
+        document.getElementById('detallesacademicosbrighton').style.display = 'block';
+        document.getElementById('masinfobrighton').style.display = 'none';
+        document.getElementById('separacion').style.display = 'none';
+        }
+    
+document.getElementById('menosinfobrighton').onclick=function(){
+            console.log("Ocultar info de la carrera del colegio Brighton")
+            document.getElementById('detallesacademicosbrighton').style.display = 'none';
+            document.getElementById('masinfobrighton').style.display = 'block';
+        }
+
+
+/*De acá para abajo estoy tratando de que, al seleccionar en el indice, se pinte el titulo, y,
+al hacer click en la parte principal o en otro boton, se despinte*/
+        
+document.getElementById('perfilpersonal').onclick=function(){
+    console.log("Pintar el titulo que queremos ver")
+    document.getElementById('perfil-personal').style.backgroundColor = 'rgb(255, 255, 112)';
+    document.getElementById('exp-profesional').style.backgroundColor = 'white';
+    document.getElementById('historial-academico').style.backgroundColor = 'white'
+    document.getElementById('Certificados').style.backgroundColor = 'white'
+    document.getElementById('Cursos').style.backgroundColor = 'white'
+    document.getElementById('idiomas').style.backgroundColor = 'white'
+}
+
+document.getElementById('expprofesional').onclick=function(){
+    console.log("Pintar el titulo que queremos ver")
+    document.getElementById('exp-profesional').style.backgroundColor = 'rgb(255, 255, 112)';
+    document.getElementById('perfil-personal').style.backgroundColor = 'white';
+    document.getElementById('historial-academico').style.backgroundColor = 'white'
+    document.getElementById('Certificados').style.backgroundColor = 'white'
+    document.getElementById('Cursos').style.backgroundColor = 'white'
+    document.getElementById('idiomas').style.backgroundColor = 'white'
+}
+
+document.getElementById('historialacademico').onclick=function(){
+    console.log("Pintar el titulo que queremos ver")
+    document.getElementById('historial-academico').style.backgroundColor = 'rgb(255, 255, 112)';
+    document.getElementById('perfil-personal').style.backgroundColor = 'white';
+    document.getElementById('exp-profesional').style.backgroundColor = 'white'
+    document.getElementById('Certificados').style.backgroundColor = 'white'
+    document.getElementById('Cursos').style.backgroundColor = 'white'
+    document.getElementById('idiomas').style.backgroundColor = 'white'
+}
+
+document.getElementById('botoncertificados').onclick=function(){
+    console.log("Pintar el titulo que queremos ver")
+    document.getElementById('Certificados').style.backgroundColor = 'rgb(255, 255, 112)';
+    document.getElementById('perfil-personal').style.backgroundColor = 'white';
+    document.getElementById('historial-academico').style.backgroundColor = 'white'
+    document.getElementById('exp-profesional').style.backgroundColor = 'white'
+    document.getElementById('Cursos').style.backgroundColor = 'white'
+    document.getElementById('idiomas').style.backgroundColor = 'white'
+}
+
+document.getElementById('botoncursos').onclick=function(){
+    console.log("Pintar el titulo que queremos ver")
+    document.getElementById('Cursos').style.backgroundColor = 'rgb(255, 255, 112)';
+    document.getElementById('perfil-personal').style.backgroundColor = 'white';
+    document.getElementById('historial-academico').style.backgroundColor = 'white'
+    document.getElementById('Certificados').style.backgroundColor = 'white'
+    document.getElementById('exp-profesional').style.backgroundColor = 'white'
+    document.getElementById('idiomas').style.backgroundColor = 'white'
+}
+
+document.getElementById('botonidiomas').onclick=function(){
+    console.log("Pintar el titulo que queremos ver")
+    document.getElementById('idiomas').style.backgroundColor = 'rgb(255, 255, 112)';
+    document.getElementById('perfil-personal').style.backgroundColor = 'white';
+    document.getElementById('historial-academico').style.backgroundColor = 'white'
+    document.getElementById('Certificados').style.backgroundColor = 'white'
+    document.getElementById('Cursos').style.backgroundColor = 'white'
+    document.getElementById('exp-profesional').style.backgroundColor = 'white'
+}
+
+
+    document.getElementById('main').onclick=function(){
+        console.log("Despintamos el titulo")
+        document.getElementById('exp-profesional').style.backgroundColor = 'white';
+        document.getElementById('perfil-personal').style.backgroundColor = 'white';
+        document.getElementById('historial-academico').style.backgroundColor = 'white'
+        document.getElementById('Certificados').style.backgroundColor = 'white'
+        document.getElementById('Cursos').style.backgroundColor = 'white'
+        document.getElementById('idiomas').style.backgroundColor = 'white'
+    }
+
+
